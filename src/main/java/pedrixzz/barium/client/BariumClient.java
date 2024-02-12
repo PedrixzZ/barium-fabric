89a856a27831ec9f0e2b29890fa1ed7239f4eb11
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-11 22:45:13 -0300

    feat: add entrypoint

---
+++ src/main/java/pedrixzz/barium/client/BariumClient.java
@@ -0,0 +1,12 @@
+package pedrixzz.barium.client;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModExemplo implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // Registre seus eventos, blocos, entidades, etc. aqui.
+        System.out.println("Barium carregado!");
+    }
+}
