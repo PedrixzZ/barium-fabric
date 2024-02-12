bfd5a3c2f78edd03dbf24fda918ed0b165edc21e
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-12 12:13:35 -0300

    Test

--- src/main/java/pedrixzz/barium/mixin/render/MinecraftRendererMixin.java
+++ src/main/java/pedrixzz/barium/mixin/render/MinecraftRendererMixin.java
@@ -3,40 +3,15 @@ package pedrixzz.barium.mixin.render;
 import pedrixzz.barium.client.render.MinecraftRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Overwrite;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.MixinEnvironment;
-import org.spongepowered.asm.mixin.Side;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.Accessor;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftRenderer.class)
 public abstract class MinecraftRendererMixin {
 
-    @Inject
-    private void inject(RendererAccessor renderer) {
-        // Injetar código aqui
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRender(CallbackInfo ci) {
+        // Chamar o método "renderWorld" do MinecraftRenderer
+        MinecraftRenderer renderer = (MinecraftRenderer) (Object) this;
+        renderer.renderWorld();
     }
-
-    @Overwrite
-    public void render() {
-        // Substituir método render()
-        
-        // Chamar método original
-        super.render();
-    }
-
-    @Redirect(method = "updateVertexBuffer", at = @At(value = "INVOKE", target = "java/util/List.addAll(Ljava/util/Collection;)Z"))
-    private boolean onAddAllVertices(List<Vertex> vertices, Collection<Vertex> newVertices) {
-        // Modificar lista de vértices antes de adicionar
-        
-        // Retornar true para adicionar os vértices modificados
-        return true;
-    }
-
-    @Accessor
-    public abstract List<Chunk> getChunks();
-
-    @Accessor
-    public abstract void setChunks(List<Chunk> chunks);
 }
