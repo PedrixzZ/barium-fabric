dc020c40f9f923ba24336febad946ae2bb1e5e02
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-11 23:16:03 -0300

    Update Barium.java

--- src/main/java/pedrixzz/barium/Barium.java
+++ src/main/java/pedrixzz/barium/Barium.java
@@ -2,11 +2,21 @@ package pedrixzz.barium;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Barium implements ModInitializer {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    @Override
-    public void onInitialize() {
-        // Registre seus eventos, blocos, entidades, etc. aqui.
-        System.out.println("Barium carregado!");
-    }
+public class Barium implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("barium");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
 }
