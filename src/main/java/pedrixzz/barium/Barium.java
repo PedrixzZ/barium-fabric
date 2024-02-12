8e7a64c257c4316852bdaa1d3d5954d77a548790
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-11 23:06:41 -0300

    test

--- src/main/java/pedrixzz/barium/Barium.java
+++ src/main/java/pedrixzz/barium/Barium.java
@@ -1,10 +1,12 @@
 package pedrixzz.barium;
 
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.fabricmc.api.ModInitializer;
 
-public class Barium implements PreLaunchEntrypoint {
+public class BariumClient implements ModInitializer {
+
     @Override
-    public void onPreLaunch() {
+    public void onInitialize() {
+        // Registre seus eventos, blocos, entidades, etc. aqui.
         System.out.println("Barium carregado!");
     }
 }
