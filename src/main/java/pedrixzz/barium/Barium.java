363e949d3d07cafc76456f693d037a8f1c12e496
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-12 21:27:29 -0300

    Update Barium.java

--- src/main/java/pedrixzz/barium/Barium.java
+++ src/main/java/pedrixzz/barium/Barium.java
@@ -9,8 +9,9 @@ public class Barium implements ModInitializer {
     public static final String MOD_ID = "barium";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-
+    @Override
+    public void onInitialize() {
+	    
+         MinecraftRenderer.registerMinecraftRenderer();
 	}
 }
