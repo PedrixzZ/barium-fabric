3ec453b131563d1dca76d8109b43128a589840a9
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-13 20:26:05 -0300

    Update WorldRendererMixin.java

--- src/main/java/pedrixzz/barium/mixin/render/WorldRendererMixin.java
+++ src/main/java/pedrixzz/barium/mixin/render/WorldRendererMixin.java
@@ -20,16 +20,6 @@ public class WorldRendererMixin {
 
     @Inject(method = "render", at = @At("HEAD"))
     private void onRenderStart(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer, boolean checkSides, CallbackInfo info) {
-        if (state.isSolid()) {
-            buffer.begin(VertexFormat.DEFAULT, Tessellator.State.POSITION_COLOR);
-            buffer.vertex(pos.getX(), pos.getY(), pos.getZ(), Colors.WHITE.getPacked());
-            buffer.vertex(pos.getX() + 1, pos.getY(), pos.getZ(), Colors.WHITE.getPacked());
-            buffer.vertex(pos.getX() + 1, pos.getY() + 1, pos.getZ(), Colors.WHITE.getPacked());
-            buffer.vertex(pos.getX(), pos.getY() + 1, pos.getZ(), Colors.WHITE.getPacked());
-            buffer.end();
-            info.cancel();
-            return;
-        }
     }
 
     @Inject(method = "render", at = @At("TAIL"))
