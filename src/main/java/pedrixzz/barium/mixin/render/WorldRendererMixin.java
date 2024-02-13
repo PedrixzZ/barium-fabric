88931c76d01434f7e0a14aea9c153c13040196f8
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-13 20:30:58 -0300

    Update WorldRendererMixin.java

--- src/main/java/pedrixzz/barium/mixin/render/WorldRendererMixin.java
+++ src/main/java/pedrixzz/barium/mixin/render/WorldRendererMixin.java
@@ -18,11 +18,59 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 
+    private static final boolean RENDER_DEBUG_OUTLINE = false;
+
     @Inject(method = "render", at = @At("HEAD"))
     private void onRenderStart(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer, boolean checkSides, CallbackInfo info) {
+        if (RENDER_DEBUG_OUTLINE) {
+            renderDebugOutline(view, state, pos, matrix, buffer);
+        }
     }
 
     @Inject(method = "render", at = @At("TAIL"))
     private void onRenderEnd(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer, boolean checkSides, CallbackInfo info) {
+        if (RENDER_DEBUG_OUTLINE) {
+            renderDebugOutline(view, state, pos, matrix, buffer);
+        }
+    }
+
+    private void renderDebugOutline(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer) {
+        World world = view.getWorld();
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+
+        // Outline the block in red
+        renderOutline(buffer, pos, Colors.RED);
+
+        // If the block has a BlockEntity, outline it in green
+        if (blockEntity != null) {
+            renderOutline(buffer, pos, Colors.GREEN);
+        }
+    }
+
+    private void renderOutline(BufferBuilder buffer, BlockPos pos, int color) {
+        float xMin = pos.getX() - 0.005f;
+        float yMin = pos.getY() - 0.005f;
+        float zMin = pos.getZ() - 0.005f;
+        float xMax = pos.getX() + 1.005f;
+        float yMax = pos.getY() + 1.005f;
+        float zMax = pos.getZ() + 1.005f;
+
+        buffer.begin(VertexFormat.DrawMode.LINES, VertexFormat.DEFAULT);
+        buffer.vertex(xMin, yMin, zMin).color(color).next();
+        buffer.vertex(xMax, yMin, zMin).color(color).next();
+        buffer.vertex(xMax, yMin, zMax).color(color).next();
+        buffer.vertex(xMin, yMin, zMax).color(color).next();
+        buffer.vertex(xMin, yMin, zMin).color(color).next();
+        buffer.vertex(xMin, yMax, zMin).color(color).next();
+        buffer.vertex(xMax, yMax, zMin).color(color).next();
+        buffer.vertex(xMax, yMin, zMin).color(color).next();
+        buffer.vertex(xMax, yMax, zMax).color(color).next();
+        buffer.vertex(xMax, yMin, zMax).color(color).next();
+        buffer.vertex(xMin, yMax, zMax).color(color).next();
+        buffer.vertex(xMin, yMin, zMax).color(color).next();
+        buffer.vertex(xMin, yMax, zMin).color(color).next();
+        buffer.vertex(xMin, yMax, zMax).color(color).next();
+        buffer.vertex(xMax, yMax, zMax).color(color).next();
+        buffer.end();
     }
 }
