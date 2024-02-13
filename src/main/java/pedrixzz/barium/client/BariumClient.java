f693df9947ca4a7030871194a79d8a9ec1c768a1
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-12 22:03:34 -0300

    Fix

--- src/main/java/pedrixzz/barium/client/BariumClient.java
+++ src/main/java/pedrixzz/barium/client/BariumClient.java
@@ -1,10 +1,12 @@
 package pedrixzz.barium.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import pedrixzz.barium.client.render.MinecraftRenderer;
 
 public class BariumClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		
+		MinecraftRenderer.registerMinecraftRenderer();
 	}
 }
