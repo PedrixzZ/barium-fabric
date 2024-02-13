e93b9fbfa7396a0724bad297cc3c30e4b2e189c2
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-13 16:11:48 -0300

    Update BariumClient.java

--- src/main/java/pedrixzz/barium/client/BariumClient.java
+++ src/main/java/pedrixzz/barium/client/BariumClient.java
@@ -1,7 +1,6 @@
 package pedrixzz.barium.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import pedrixzz.barium.client.render.WorldRenderer;
 
 public class BariumClient implements ClientModInitializer {
 	@Override
