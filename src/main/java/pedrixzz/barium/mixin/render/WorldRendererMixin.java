652a4ca6e8006254441bf648661d726ebe02bfe3
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-13 20:17:23 -0300

    Test

--- src/main/java/pedrixzz/barium/mixin/render/WorldRendererMixin.java
+++ src/main/java/pedrixzz/barium/mixin/render/WorldRendererMixin.java
@@ -1,33 +1,37 @@
 package pedrixzz.barium.mixin.render;
 
+import net.minecraft.world.BlockRenderView;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.Frustum;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import org.joml.Matrix4f;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
-public abstract class WorldRendererMixin {
-
-    @Shadow private int chunkIndexY;
-    @Shadow private int chunkIndexZ;
-    @Shadow private int chunkIndexX;
+public class WorldRendererMixin {
 
     @Inject(method = "render", at = @At("HEAD"))
-    private void onRenderStart(RenderLayer layer, Frustum frustum, double cameraX, double cameraY, double cameraZ, MatrixStack projectionMatrix, MatrixStack viewMatrix, Entity viewEntity, boolean isWorldRenderer, int chunkIndexX, int chunkIndexY, int chunkIndexZ, CallbackInfo info) {
-        // Desativar a renderização de blocos transparentes se não forem visíveis
-        if (layer == RendeLayer.TRANSLUCENT) {
-            if (!frustum.isBoundingBoxVisible(BlockBounds.of(chunkIndexX * 16, chunkIndexY * 16, chunkIndexZ * 16, (chunkIndexX + 1) * 16, (chunkIndexY + 1) * 16, (chunkIndexZ + 1) * 16))) {
-                info.cancel();
-                return;
-            }
+    private void onRenderStart(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer, boolean checkSides, CallbackInfo info) {
+        if (state.isSolid()) {
+            buffer.begin(VertexFormat.DEFAULT, Tessellator.State.POSITION_COLOR);
+            buffer.vertex(pos.getX(), pos.getY(), pos.getZ(), Color.WHITE.getPacked());
+            buffer.vertex(pos.getX() + 1, pos.getY(), pos.getZ(), Color.WHITE.getPacked());
+            buffer.vertex(pos.getX() + 1, pos.getY() + 1, pos.getZ(), Color.WHITE.getPacked());
+            buffer.vertex(pos.getX(), pos.getY() + 1, pos.getZ(), Color.WHITE.getPacked());
+            buffer.end();
+            info.cancel();
+            return;
         }
+    }
+
+    @Inject(method = "render", at = @At("TAIL"))
+    private void onRenderEnd(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer, boolean checkSides, CallbackInfo info) {
     }
 }
