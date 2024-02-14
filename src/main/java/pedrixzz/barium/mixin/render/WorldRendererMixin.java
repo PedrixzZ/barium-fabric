2c0b2713081d70bc8f3e6b8dfa1673c8bf342e56
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-13 21:05:00 -0300

    Update WorldRendererMixin.java

--- src/main/java/pedrixzz/barium/mixin/render/WorldRendererMixin.java
+++ src/main/java/pedrixzz/barium/mixin/render/WorldRendererMixin.java
@@ -18,51 +18,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 
-    private static final boolean RENDER_DEBUG_OUTLINE = false;
+    private static final Matrix4f MATRIX4F_IDENTITY = new Matrix4f().identity();
+    private static final VertexFormat VERTEX_FORMAT = VertexFormat.Sprite;
+
+    private BufferBuilder vertexBuffer;
 
     @Inject(method = "render", at = @At("HEAD"))
     private void onRenderStart(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer, boolean checkSides, CallbackInfo info) {
-        if (RENDER_DEBUG_OUTLINE) {
-            renderDebugOutline(view, state, pos, matrix, buffer);
-        }
+        this.vertexBuffer = buffer.dynamic(VERTEX_FORMAT);
     }
 
     @Inject(method = "render", at = @At("TAIL"))
     private void onRenderEnd(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer, boolean checkSides, CallbackInfo info) {
-        if (RENDER_DEBUG_OUTLINE) {
-            renderDebugOutline(view, state, pos, matrix, buffer);
+        if (vertexBuffer.getVertexCount() > 0) {
+            renderBuffer(view, state, pos, vertexBuffer);
         }
     }
 
-    private void renderDebugOutline(BlockRenderView view, BlockState state, BlockPos pos, Matrix4f matrix, BufferBuilder buffer) {
-        World world = view.getRenderer().getWorld();
-        BlockEntity blockEntity = world.getBlockEntity(pos);
-    }
-
-    private void renderOutline(BufferBuilder buffer, BlockPos pos, int color) {
-        float xMin = pos.getX() - 0.005f;
-        float yMin = pos.getY() - 0.005f;
-        float zMin = pos.getZ() - 0.005f;
-        float xMax = pos.getX() + 1.005f;
-        float yMax = pos.getY() + 1.005f;
-        float zMax = pos.getZ() + 1.005f;
-
-        buffer.begin(VertexFormat.DrawMode.LINES);
-        buffer.vertex(xMin, yMin, zMin).color(color).next();
-        buffer.vertex(xMax, yMin, zMin).color(color).next();
-        buffer.vertex(xMax, yMin, zMax).color(color).next();
-        buffer.vertex(xMin, yMin, zMax).color(color).next();
-        buffer.vertex(xMin, yMin, zMin).color(color).next();
-        buffer.vertex(xMin, yMax, zMin).color(color).next();
-        buffer.vertex(xMax, yMax, zMin).color(color).next();
-        buffer.vertex(xMax, yMin, zMin).color(color).next();
-        buffer.vertex(xMax, yMax, zMax).color(color).next();
-        buffer.vertex(xMax, yMin, zMax).color(color).next();
-        buffer.vertex(xMin, yMax, zMax).color(color).next();
-        buffer.vertex(xMin, yMin, zMax).color(color).next();
-        buffer.vertex(xMin, yMax, zMin).color(color).next();
-        buffer.vertex(xMin, yMax, zMax).color(color).next();
-        buffer.vertex(xMax, yMax, zMax).color(color).next();
-        buffer.end();
+    private void renderBuffer(BlockRenderView view, BlockState state, BlockPos pos, BufferBuilder buffer) {
+        buffer.endVertex();
+        WorldRenderer.draw(buffer);
     }
 }
