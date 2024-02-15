930f67aab0f0acaf9a11551bcfe8918e1e0c144e
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-15 20:34:19 -0300

    Update MixinWorldRenderer.java

--- src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
+++ src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
@@ -1,24 +1,27 @@
 package pedrixzz.barium.mixin.render;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRendererAccessor;
-import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
-@Environment(EnvType.CLIENT)
-public class MixinWorldRenderer {
+public abstract class PerformanceMixin {
 
-    @Inject(method = "render", at = @At("HEAD"))
-    private void onRender(CallbackInfo info) {
-        // Força o uso do OpenGL 2.0
-        ((WorldRendererAccessor) this).getRenderGlobal().setShader(
-            ((WorldRendererAccessor) this).getRenderGlobal().getShaderResourceManager().getShader("minecraft:legacy")
-        );
+    @Inject
+    @At(value = "HEAD", target = "renderWorld(Lnet/minecraft/client/render/Camera; FLnet/minecraft/world/World; Lnet/minecraft/util/math/BlockPos; Lnet/minecraft/client/util/math/MatrixStack; F)V")
+    private void onRenderWorldStart(Camera camera, float partialTicks, World world, BlockPos pos, MatrixStack matrices, float alpha, Callback callback) {
+        // Insira seu código de otimização aqui
+    }
+
+    @Inject
+    @At(value = "TAIL", target = "renderWorld(Lnet/minecraft/client/render/Camera; FLnet/minecraft/world/World; Lnet/minecraft/util/math/BlockPos; Lnet/minecraft/client/util/math/MatrixStack; F)V")
+    private void onRenderWorldEnd(Camera camera, float partialTicks, World world, BlockPos pos, MatrixStack matrices, float alpha, Callback callback) {
+        // Insira seu código de limpeza aqui
     }
 }
