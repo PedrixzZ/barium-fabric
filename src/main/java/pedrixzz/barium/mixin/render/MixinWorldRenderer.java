76853d7b4f07602358649bf075a2c62302553aef
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-15 20:20:30 -0300

    Update MixinWorldRenderer.java

--- src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
+++ src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
@@ -1,41 +1,24 @@
 package pedrixzz.barium.mixin.render;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRendererAccessor;
+import net.minecraft.client.renderer.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import net.minecraft.client.render.WorldRenderer;
-
-import org.lwjgl.opengl.GL11;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
-public abstract class MixinWorldRenderer {
+@Environment(EnvType.CLIENT)
+public class MixinWorldRenderer {
 
-    @Shadow private static int CHUNK_SIZE;
-
-    @Inject
-    public MixinWorldRenderer(WorldRenderer worldRenderer) {
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRender(CallbackInfo info) {
+        // Força o uso do OpenGL 2.0
+        ((WorldRendererAccessor) this).getRenderGlobal().setShader(
+            ((WorldRendererAccessor) this).getRenderGlobal().getShaderResourceManager().getShader("minecraft:legacy")
+        );
     }
-
-    @ModifyArg(method = "render", at = @At("HEAD"))
-    private int modifyRender(int pass) {
-        if (pass == 0) {
-            // Ativar OpenGL 2.0
-            GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-            GL11.glEnableClientState(GL11.GL_COLOR_ARRAY);
-            GL11.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-        }
-        return pass;
-    }
-
-    @ModifyArg(method = "renderBlock", at = @At("TAIL"))
-    private int modifyRenderBlock(int vertexCount) {
-        // Otimizar renderização de blocos
-        if (vertexCount > CHUNK_SIZE * CHUNK_SIZE * 6) {
-            return CHUNK_SIZE * CHUNK_SIZE * 6;
-        }
-        return vertexCount;
-    }
-
 }
