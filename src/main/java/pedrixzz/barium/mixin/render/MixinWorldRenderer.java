1b36dba509442fdb7e62ec3831620a92b2a77348
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-15 22:18:46 -0300

    Test

--- src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
+++ src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
@@ -2,52 +2,54 @@ package pedrixzz.barium.mixin.render;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.api.mixin.Mixin;
-import net.fabricmc.api.mixin.injection.At;
-import net.fabricmc.api.mixin.injection.Inject;
-import net.fabricmc.api.mixin.injection.Redirect;
-import net.minecraft.block.BlockState;
-import net.minecraft.client.render.Frustum;
-import net.minecraft.client.render.chunk.Chunk;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockLightSubtractions;
-import net.minecraft.world.LightmapTextureManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.block.BlockRenderLayer;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.util.math.Matrix4f;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.LightmapTextureManager;
 
 import org.lwjgl.opengl.GL11;
 
-@Mixin(Chunk.class)
-public abstract class ChunkMixin {
+@Mixin(WorldRenderer.class)
+public abstract class MixinWorldRenderer {
 
-    @Environment(EnvType.CLIENT)
     @Inject(method = "render", at = @At("HEAD"))
-    private void onRenderStart(Frustum frustum, double camX, double camY, double camZ, Matrix4f projectionMatrix, Matrix4f viewMatrix, int renderPass, CallbackInfo info) {
-        // Otimização: Desativar a renderização de faces ocultas
-        GlStateManager.disableCull();
+    private void onRenderStart(
+        BlockRenderLayer layer,
+        Matrix4f matrix,
+        double cameraX,
+        double cameraY,
+        double cameraZ,
+        LightmapTextureManager lightmapTextureManager,
+        int light,
+        WorldRenderer.FinishCallback callback
+    ) {
+        // Insira seu código de otimização OpenGL 2.0 aqui.
 
-        // Otimização: Desativar o teste de profundidade
-        GlStateManager.disableDepthTest();
-
-        // Otimização: Usar alpha blending para translucidez
-        GlStateManager.enableAlphaTest();
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        // Exemplo: desabilitar o antialiasing
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        GL11.glDisable(GL11.GL_POLYGON_SMOOTH);
     }
 
-    @Environment(EnvType.CLIENT)
-    @Inject(method = "getLightmap", at = @At("RETURN"))
-    private void onGetLightmap(BlockState state, int x, int y, int z, BlockLightSubtractions subtractions, @Nullable LightmapTextureManager lightmap, CallbackInfoReturnable<Int2> info) {
-        // Otimização: Usar um valor de luzmap constante para blocos opacos
-        if (state.isOpaque()) {
-            info.setReturnValue(new Int2(15, 15));
-        }
-    }
+    @Inject(method = "render", at = @At("TAIL"))
+    private void onRenderEnd(
+        BlockRenderLayer layer,
+        Matrix4f matrix,
+        double cameraX,
+        double cameraY,
+        double cameraZ,
+        LightmapTextureManager lightmapTextureManager,
+        int light,
+        WorldRenderer.FinishCallback callback
+    ) {
+        // Reative o antialiasing se você o desabilitou no início.
 
-    @Environment(EnvType.CLIENT)
-    @Redirect(method = "isOpaque", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOpaque()Z"))
-    private boolean onIsOpaque(BlockState state) {
-        // Otimização: Retornar sempre true para blocos que não precisam de translucidez
-        return true;
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glEnable(GL11.GL_POLYGON_SMOOTH);
     }
-
 }
