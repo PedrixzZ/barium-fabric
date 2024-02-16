29a3204be694de50cf134eb130193a8b01b6c823
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-15 22:48:35 -0300

    Update MixinWorldRenderer.java

--- src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
+++ src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
@@ -1,55 +1,49 @@
 package pedrixzz.barium.mixin.render;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.render.BlockRenderView;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.Matrix4f;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.block.BlockRenderLayer;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.util.math.Matrix4f;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.LightmapTextureManager;
 
-import org.lwjgl.opengl.GL11;
-
-@Mixin(WorldRenderer.class)
-public abstract class MixinWorldRenderer {
-
-    @Inject(method = "render", at = @At("HEAD"))
-    private void onRenderStart(
-        BlockRenderLayer layer,
-        Matrix4f matrix,
-        double cameraX,
-        double cameraY,
-        double cameraZ,
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin {
+    @Inject(method = "renderWorld", at = @At("HEAD"))
+    private void onRenderWorldStart(
+        @Nullable IBlockState state,
+        BlockRenderView entityView,
+        BlockStateView blockStateView,
+        WorldRenderer worldRenderer,
         LightmapTextureManager lightmapTextureManager,
-        int light,
-        WorldRenderer.FinishCallback callback
-    ) {
-        // Insira seu código de otimização OpenGL 2.0 aqui.
-
-        // Exemplo: desabilitar o antialiasing
-        GL11.glDisable(GL11.GL_LINE_SMOOTH);
-        GL11.glDisable(GL11.GL_POLYGON_SMOOTH);
+        Matrix4f matrix4f,
+        int i,
+        int j,
+        int k,
+        Camera camera) {
+        // Insira seu código de otimização aqui
+        // Por exemplo, você pode desabilitar a renderização de entidades
+        // ou reduzir a qualidade da luz.
     }
 
-    @Inject(method = "render", at = @At("TAIL"))
-    private void onRenderEnd(
-        BlockRenderLayer layer,
-        Matrix4f matrix,
-        double cameraX,
-        double cameraY,
-        double cameraZ,
+    @Inject(method = "renderWorld", at = @At("TAIL"))
+    private void onRenderWorldEnd(
+        @Nullable IBlockState state,
+        BlockRenderView entityView,
+        BlockStateView blockStateView,
+        WorldRenderer worldRenderer,
         LightmapTextureManager lightmapTextureManager,
-        int light,
-        WorldRenderer.FinishCallback callback
-    ) {
-        // Reative o antialiasing se você o desabilitou no início.
-
-        GL11.glEnable(GL11.GL_LINE_SMOOTH);
-        GL11.glEnable(GL11.GL_POLYGON_SMOOTH);
+        Matrix4f matrix4f,
+        int i,
+        int j,
+        int k,
+        Camera camera) {
+        // Insira seu código de limpeza aqui
     }
 }
