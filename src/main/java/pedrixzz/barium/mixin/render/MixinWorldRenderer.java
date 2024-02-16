10821645698e1c1012989be6945df5721af362a9
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-15 22:13:38 -0300

    Test

--- src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
+++ src/main/java/pedrixzz/barium/mixin/render/MixinWorldRenderer.java
@@ -1,27 +1,53 @@
 package pedrixzz.barium.mixin.render;
 
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.util.math.MatrixStack;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.mixin.Mixin;
+import net.fabricmc.api.mixin.injection.At;
+import net.fabricmc.api.mixin.injection.Inject;
+import net.fabricmc.api.mixin.injection.Redirect;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.chunk.Chunk;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import net.minecraft.world.BlockLightSubtractions;
+import net.minecraft.world.LightmapTextureManager;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(WorldRenderer.class)
-public abstract class MixinWorldRenderer {
+import org.lwjgl.opengl.GL11;
 
-    @Inject
-    @At(value = "HEAD", target = "renderWorld(Lnet/minecraft/client/render/Camera; FLnet/minecraft/world/World; Lnet/minecraft/util/math/BlockPos; Lnet/minecraft/client/util/math/MatrixStack; F)V")
-    private void onRenderWorldStart(Camera camera, float partialTicks, World world, BlockPos pos, MatrixStack matrices, float alpha, Callback callback) {
-        // Insira seu código de otimização aqui
+@Mixin(Chunk.class)
+public abstract class ChunkMixin {
+
+    @Environment(EnvType.CLIENT)
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRenderStart(Frustum frustum, double camX, double camY, double camZ, Matrix4f projectionMatrix, Matrix4f viewMatrix, int renderPass, CallbackInfo info) {
+        // Otimização: Desativar a renderização de faces ocultas
+        GlStateManager.disableCull();
+
+        // Otimização: Desativar o teste de profundidade
+        GlStateManager.disableDepthTest();
+
+        // Otimização: Usar alpha blending para translucidez
+        GlStateManager.enableAlphaTest();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
     }
 
-    @Inject
-    @At(value = "TAIL", target = "renderWorld(Lnet/minecraft/client/render/Camera; FLnet/minecraft/world/World; Lnet/minecraft/util/math/BlockPos; Lnet/minecraft/client/util/math/MatrixStack; F)V")
-    private void onRenderWorldEnd(Camera camera, float partialTicks, World world, BlockPos pos, MatrixStack matrices, float alpha, Callback callback) {
-        // Insira seu código de limpeza aqui
+    @Environment(EnvType.CLIENT)
+    @Inject(method = "getLightmap", at = @At("RETURN"))
+    private void onGetLightmap(BlockState state, int x, int y, int z, BlockLightSubtractions subtractions, @Nullable LightmapTextureManager lightmap, CallbackInfoReturnable<Int2> info) {
+        // Otimização: Usar um valor de luzmap constante para blocos opacos
+        if (state.isOpaque()) {
+            info.setReturnValue(new Int2(15, 15));
+        }
     }
+
+    @Environment(EnvType.CLIENT)
+    @Redirect(method = "isOpaque", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOpaque()Z"))
+    private boolean onIsOpaque(BlockState state) {
+        // Otimização: Retornar sempre true para blocos que não precisam de translucidez
+        return true;
+    }
+
 }
